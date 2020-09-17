8f6e9ac777252fe8c2ab1d451767f20788db7cea
Eflamm <eflamm.ollivier@gmail.com>
2020-09-17 19:05:03 +0200

    document the CustomElement class

--- src/custom-element.js
+++ src/custom-element.js
@@ -1,19 +1,24 @@
 /**
- * Add the data binding to the componnent. You have to use the 'data-bind' attribute in the tags displaying the data,
- * and 'data-model' attribute in the tags modifying the data. The value in these attributes must match the state attributes.
+ * Provides  data binding to the component. You have to use the 'data-bind' attribute on the elements displaying the data,
+ * and 'data-model' attribute on the tags modifying the data. The value in the element attributes must match the state attributes.
  */
 class CustomElement extends HTMLElement {
   /**
-   *
-   * @param {multine string} template - The HTML tags
-   * @param {json object} state - the component attributes
+   * Injects the template and the data in the component, updates the values when changes happen
+   * @param {multine string} template - The HTML tags injected in the component
+   * @param {json object} state - the data injected in the component
    */
   constructor(template, state) {
     super();
 
+    // Render the template inside the component
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.innerHTML = template;
 
+    /**
+     * Creates a Proxy with the initial values of the state, and updates the view everytime the values changes
+     * @param { json object } state - the initial values of the state
+     */
     const createState = (state) => {
       return new Proxy(state, {
         set(target, property, value) {
@@ -24,6 +29,10 @@ class CustomElement extends HTMLElement {
       });
     };
 
+    /**
+     * Updates the view with the state values. The state attributes matches the element data attributes.
+     * @param { Proxy } state - contains the data to display in the view
+     */
     const render = (state) => {
       const bindings = Array.from(
         shadowRoot.querySelectorAll("[data-bind]")
@@ -36,6 +45,9 @@ class CustomElement extends HTMLElement {
       });
     };
 
+    /**
+     * Register all the data-model elements to event listeners.
+     */
     const listeners = shadowRoot.querySelectorAll("[data-model]");
     listeners.forEach((listener) => {
       const name = listener.dataset.model;
@@ -45,8 +57,10 @@ class CustomElement extends HTMLElement {
       });
     });
 
+    // Instanciates the state
     state = createState(state);
 
+    // Render the state values in the view
     render(state);
   }
 }
