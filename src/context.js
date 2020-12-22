7be25b09b3fd4ddd65465b38cca5926e5425fde8
Eflamm <eflamm.ollivier@gmail.com>
2020-12-22 23:48:47 +0100

    WIP observe context changes

--- src/context.js
+++ src/context.js
@@ -3,8 +3,43 @@ class Context  {
 		this.token = null;
 		this.isEditorMode = true;
 		this.preferences = {};
+		this.subscribers = [];
+	}
+
+	set tokent(t) {
+		this.isEditorMode = t;
+		for(const subscriber of this.subscribers) {
+			subscriber(this.getContext());
+		}
+	}
+
+	set isEditorMode(t) {
+		this.isEditorMode = t;
+		for(const subscriber of this.subscribers) {
+			subscriber(this.getContext());
+		}
+	}
+
+	set preferences(t) {
+		this.isEditorMode = t;
+		for(const subscriber of this.subscribers) {
+			subscriber(this.getContext());
+		}
+	}
+	
+	getContext(){
+		return {
+			token: this.token, 
+			isEditorMode: this.isEditorMode,
+			preferences: this.preferences
+		};
+	}
+
+	subscribe(subscriberFunction) {
+		this.subscribers.push(subscriberFunction);
 	}
 }
+
 
 let context = new Context();
 
